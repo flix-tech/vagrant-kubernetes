adab36dfec093bc0f503607fbb5429734185a286
Steffen Hanikel <steffen.hanikel@meinfernbus.de>
2016-09-27 14:48:56 +0200

    Remove rsynced files before packaging

--- vg-02-reformat.sh
+++ vg-02-reformat.sh
@@ -8,6 +8,7 @@ SWAPUUID=$(blkid /dev/sda2 -s UUID | cut -f 2 -d '=' | tr -d '"' )
 echo "UUID=${SWAPUUID} none            swap    sw              0       0" >> /etc/fstab
 
 rm -rf /var/log/installer/
+find /vagrant/ -mindepth 1 -delete
 
 # Clean up unused space
 dd if=/dev/zero of=/EMPTY bs=64k || true
@@ -21,8 +22,5 @@ resize2fs /dev/sda1 60G
 # Reset to vagrant insecure key
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" \
 > /home/vagrant/.ssh/authorized_keys
-
-umount -i /vagrant
-rm -rf /vagrant/* /vagrant/.*
 
 systemctl poweroff
